c90ff5ba9c1708f5bb708aa1de0c6b400ec636ba
udcyber <udewaleyne.pro@gmail.com>
2026-04-10 14:39:19 +0200

    betty code

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -4,7 +4,7 @@
  * binary_tree_is_root - check if node is root
  * @node: node to check
  * Return:	1 if node is root
-			0 if not or is NULL
+ *			0 if not or is NULL
  */
 
 int binary_tree_is_root(const binary_tree_t *node)
