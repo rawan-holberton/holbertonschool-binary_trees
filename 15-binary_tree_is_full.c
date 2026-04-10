ecc1d258ca4dee4ca8f89d5be9d430de1915866f
udcyber <udewaleyne.pro@gmail.com>
2026-04-10 14:37:51 +0200

    betty code verification

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -17,7 +17,8 @@ int binary_tree_is_full(const binary_tree_t *tree)
 		if (binary_tree_is_full(tree->left) != 1)
 			return (0);
 	}
-	if ((tree->left && tree->right == NULL) || (tree->left == NULL && tree->right))
+	if ((tree->left && tree->right == NULL)
+	|| (tree->left == NULL && tree->right))
 		return (0);
 
 	return (1);
